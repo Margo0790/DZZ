4820401ac161220f20d17cdc1cb53b2f5284fbb9
neto test <ovsepyanm@yahoo.com>
2023-06-21 00:47:21 +0300

    file modified

--- src/test/java/ru/netology/web/OrderingCardTest.java
+++ src/test/java/ru/netology/web/OrderingCardTest.java
@@ -1,6 +1,6 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,7 +9,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
